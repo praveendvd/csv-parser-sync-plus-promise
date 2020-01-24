5d70cbae77a08fc9ca9aa227920efa79cdf3bc69
Praveen Mathew <S2CN@eurofins.com>
2020-01-24 14:47:44 +0000

    added rejection

--- readSync.js
+++ readSync.js
@@ -1,39 +1,42 @@
 var sleep = require('system-sleep');
 
-module.exports={
-  readCsvPromise:function (path1) {
-console.log(path1)
-  const csv = require('csv-parser')
-  const fs = require('fs')
-  const results = [];
+module.exports = {
+  readCsvPromise: function (path1) {
+    console.log(path1)
+    const csv = require('csv-parser')
+    const fs = require('fs')
+    const results = [];
 
 
-  function getIdRequest(path2) {
-    console.log(path2)
-    return new Promise((resolve, reject) => {
+    function getIdRequest(path2) {
+      console.log(path2)
+      return new Promise((resolve, reject) => {
 
-     
-      fs.createReadStream(path2)
-        .pipe(csv(path2))
-        .on('data', (data) => results.push(data))
-        .on('end', () => {
-          resolve(results);
-        })
-    });
+
+        fs.createReadStream(path2)
+          .on('error', function (err) {
+            reject(err);
+          })
+          .pipe(csv(path2))
+          .on('data', (data) => results.push(data))
+          .on('end', () => {
+            resolve(results);
+          })
+      });
+    }
+    console.log(path1)
+    return getIdRequest(path1);
+  },
+
+
+  readCsvSync: function (path1) {
+    let testdata = this.readCsvPromise(path1)
+    testdata.then(function (b) { testdata = b }).catch(function (c) { console.error(c); process.exit(222) });
+    while (typeof testdata.then === 'function') {
+      sleep(1)
+    }
+    return (testdata)
+
   }
-  console.log(path1)
-  return getIdRequest(path1);
-},
-
-
-readCsvSync:function (path1) {
-  let testdata=this.readCsvPromise(path1)
-  testdata.then(function(b){testdata=b}).catch(function(c){ console.error(c); process.exit(222)});
-  while(typeof testdata.then === 'function'){
-    sleep(1)
-  }
-return(testdata)
-
-}
 
 }
